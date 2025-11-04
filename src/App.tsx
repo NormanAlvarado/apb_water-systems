9de68767cb715ae26b2938ce17a280f5354ca5d6
Norman Alvarado <norman.alvarado.chaves@est.una.ac.cr>
2025-11-04 16:42:57 -0600

    refactor: Update routing in App component and change email contact in Temp page for consistency

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
-import Temp from './pages/Temp'
 import WaterConditioning from './pages/WaterConditioning'
 import DrinkingWater from './pages/DrinkingWater'
 import ProblemSolvingFilters from './pages/ProblemSolvingFilters'
@@ -13,8 +12,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Temp />} />
-        <Route path="/devhome" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/products/water-conditioning" element={<WaterConditioning />} />
